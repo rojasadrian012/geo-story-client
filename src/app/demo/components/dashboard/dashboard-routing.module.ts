966e943bcfe92976d3aa5f5fe12fba858a5b81d0
rojasadrian012 <rojasadrian012@gmail.com>
2024-05-18 15:03:04 -0400

    path with uuid

--- src/app/demo/components/dashboard/dashboard-routing.module.ts
+++ src/app/demo/components/dashboard/dashboard-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { LessonComponent } from './pages/lesson/lesson.component';
+import { isAuthenticatedGuard } from '../auth/guards/is-authenticated.guard';
 
 @NgModule({
     imports: [RouterModule.forChild([
         { path: '', component: DashboardComponent },
-        { path: 'lesson/:level', component: LessonComponent }
+        { path: 'lesson/:level/:id',canActivate: [isAuthenticatedGuard], component: LessonComponent }
     ])],
     exports: [RouterModule]
 })
