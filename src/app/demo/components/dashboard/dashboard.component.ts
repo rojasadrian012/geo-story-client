360569b20aba600cc7045d0a29ecf5ee1ca04042
rojasadrian012 <rojasadrian012@gmail.com>
2024-05-20 11:15:50 -0400

    initial version the home page

--- src/app/demo/components/dashboard/dashboard.component.ts
+++ src/app/demo/components/dashboard/dashboard.component.ts
@@ -7,128 +7,28 @@ import { LayoutService } from 'src/app/layout/service/app.layout.service';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { QuizListResponse } from './interfaces/quiz-list-response.interface';
+import { Router } from '@angular/router';
 
 @Component({
     templateUrl: './dashboard.component.html',
     styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent implements OnInit, OnDestroy {
-    items!: MenuItem[];
-
-    products!: Product[];
-
-    chartData: any;
-
-    chartOptions: any;
-
-    subscription!: Subscription;
+export class DashboardComponent implements OnInit {
+    private readonly http = inject(HttpClient);
+    private readonly router = inject(Router);
 
     levels: QuizListResponse[] = []
-
-    private readonly http = inject(HttpClient);
-
-    constructor(
-        private productService: ProductService,
-        public layoutService: LayoutService
-    ) {
-        this.subscription = this.layoutService.configUpdate$
-            .pipe(debounceTime(25))
-            .subscribe((config) => {
-                this.initChart();
-            });
-    }
+    images: string[] = [
+        '../../../../assets/images/curiosidades.svg',
+        '../../../../assets/images/historia.svg',
+        '../../../../assets/images/geografia.svg',
+        '../../../../assets/images/economia.svg',
+        '../../../../assets/images/cultura.svg',
+        '../../../../assets/images/turismo.svg',
+    ]
 
     ngOnInit() {
-        this.initChart();
-        this.productService
-            .getProductsSmall()
-            .then((data) => (this.products = data));
-
-        this.items = [
-            { label: 'Add New', icon: 'pi pi-fw pi-plus' },
-            { label: 'Remove', icon: 'pi pi-fw pi-minus' },
-        ];
-
         this.getQuizzes()
-    }
-
-    initChart() {
-        const documentStyle = getComputedStyle(document.documentElement);
-        const textColor = documentStyle.getPropertyValue('--text-color');
-        const textColorSecondary = documentStyle.getPropertyValue(
-            '--text-color-secondary'
-        );
-        const surfaceBorder =
-            documentStyle.getPropertyValue('--surface-border');
-
-        this.chartData = {
-            labels: [
-                'January',
-                'February',
-                'March',
-                'April',
-                'May',
-                'June',
-                'July',
-            ],
-            datasets: [
-                {
-                    label: 'First Dataset',
-                    data: [65, 59, 80, 81, 56, 55, 40],
-                    fill: false,
-                    backgroundColor:
-                        documentStyle.getPropertyValue('--bluegray-700'),
-                    borderColor:
-                        documentStyle.getPropertyValue('--bluegray-700'),
-                    tension: 0.4,
-                },
-                {
-                    label: 'Second Dataset',
-                    data: [28, 48, 40, 19, 86, 27, 90],
-                    fill: false,
-                    backgroundColor:
-                        documentStyle.getPropertyValue('--green-600'),
-                    borderColor: documentStyle.getPropertyValue('--green-600'),
-                    tension: 0.4,
-                },
-            ],
-        };
-
-        this.chartOptions = {
-            plugins: {
-                legend: {
-                    labels: {
-                        color: textColor,
-                    },
-                },
-            },
-            scales: {
-                x: {
-                    ticks: {
-                        color: textColorSecondary,
-                    },
-                    grid: {
-                        color: surfaceBorder,
-                        drawBorder: false,
-                    },
-                },
-                y: {
-                    ticks: {
-                        color: textColorSecondary,
-                    },
-                    grid: {
-                        color: surfaceBorder,
-                        drawBorder: false,
-                    },
-                },
-            },
-        };
-    }
-
-    ngOnDestroy() {
-        if (this.subscription) {
-            this.subscription.unsubscribe();
-        }
     }
 
     getQuizzes() {
@@ -136,5 +36,9 @@ export class DashboardComponent implements OnInit, OnDestroy {
             .subscribe((data) => {
                 this.levels = data
             });
+    }
+
+    changePath(level: QuizListResponse) {
+        this.router.navigateByUrl(`lesson/${level.title}/${level.id}`)
     }
 }
