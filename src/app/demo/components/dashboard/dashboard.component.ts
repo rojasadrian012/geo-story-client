eacc466535b52b3300c48142fe79964823bca35f
rojasadrian012 <rojasadrian012@gmail.com>
2024-05-27 11:08:54 -0400

    unlock only the firt element

--- src/app/demo/components/dashboard/dashboard.component.ts
+++ src/app/demo/components/dashboard/dashboard.component.ts
@@ -17,7 +17,6 @@ export class DashboardComponent implements OnInit {
     private readonly http = inject(HttpClient);
     private readonly router = inject(Router);
 
-    levels: QuizListResponse[] = []
     images: string[] = [
         '../../../../assets/images/options-images/curiosidades.svg',
         '../../../../assets/images/options-images/historia.svg',
@@ -25,22 +24,27 @@ export class DashboardComponent implements OnInit {
         '../../../../assets/images/options-images/economia.svg',
         '../../../../assets/images/options-images/cultura.svg',
         '../../../../assets/images/options-images/turismo.svg',
-    ]
-    blocked: boolean = false
+      ];
+      isBlocked: boolean = true;
+      levels: QuizListResponse[] = [];
 
-    ngOnInit() {
-        this.getQuizzes()
-    }
+      ngOnInit() {
+        this.getQuizzes();
+      }
 
-    getQuizzes() {
+      getQuizzes() {
         this.http.get<QuizListResponse[]>(environment.baseUrl + '/quiz')
-            .subscribe((data) => {
-                this.levels = data
-            });
-    }
+          .subscribe((data) => {
+            this.levels = data;
+          });
+      }
 
-    changePath(level: QuizListResponse, blocked: boolean) {
-        if(blocked) return
-        this.router.navigateByUrl(`lesson/${level.title}/${level.id}`)
-    }
+      changePath(level: QuizListResponse, blocked: boolean) {
+        if (blocked) return;
+        this.router.navigateByUrl(`lesson/${level.title}/${level.id}`);
+      }
+
+      unlockAllLevels() {
+        this.isBlocked = !this.isBlocked;
+      }
 }
