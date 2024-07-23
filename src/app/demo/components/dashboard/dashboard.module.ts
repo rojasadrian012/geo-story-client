4cffbbbea359288e3f274274d299259855ce38c9
rojasadrian012 <rojasadrian012@gmail.com>
2024-07-23 01:32:07 -0400

    clean module

--- src/app/demo/components/dashboard/dashboard.module.ts
+++ src/app/demo/components/dashboard/dashboard.module.ts
@@ -1,40 +1,21 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { DashboardComponent } from './dashboard.component';
-import { ChartModule } from 'primeng/chart';
-import { MenuModule } from 'primeng/menu';
-import { TableModule } from 'primeng/table';
-import { StyleClassModule } from 'primeng/styleclass';
-import { PanelMenuModule } from 'primeng/panelmenu';
-import { DashboardsRoutingModule } from './dashboard-routing.module';
-import { ToastModule } from 'primeng/toast';
 
-import { LessonComponent } from './pages/lesson/lesson.component';
-import { MessageService } from 'primeng/api';
-import { HintComponent } from './components/hint/hint.component';
-import { QuestionsAndAnswersComponent } from './components/questions-and-answers/questions-and-answers.component';
+import { DashboardComponent } from './dashboard.component';
+import { DashboardsRoutingModule } from './dashboard-routing.module';
 import { CategoryComponent } from './components/category/category.component';
 
 @NgModule({
     imports: [
-        CommonModule,
-        FormsModule,
-        ChartModule,
-        MenuModule,
-        TableModule,
-        StyleClassModule,
-        PanelMenuModule,
-        DashboardsRoutingModule,
-        ToastModule,
-        HintComponent,
-        QuestionsAndAnswersComponent,
+        // StyleClassModule,
+        // PanelMenuModule,
+        // ToastModule,
+        // HintComponent,
+        // QuestionsAndAnswersComponent,
         CategoryComponent,
+        DashboardsRoutingModule,
     ],
     declarations: [
         DashboardComponent,
-        LessonComponent
     ],
-    providers: [MessageService],
 })
 export class DashboardModule { }
