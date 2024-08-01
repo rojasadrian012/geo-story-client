f82eb892d7bea0e2fc8c45b9fad4d7615f8cc308
rojasadrian012 <rojasadrian012@gmail.com>
2024-08-01 14:58:06 -0400

    inicial version acheivements

--- src/app/demo/components/dashboard/pages/achievement/achievement.component.ts
+++ src/app/demo/components/dashboard/pages/achievement/achievement.component.ts
@@ -2,19 +2,35 @@ import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { GeoCenterContainerComponent } from '../../components/core/geo-center-container/geo-center-container.component';
 import { AchievementItemComponent } from '../../components/achievement-item/achievement-item.component';
 import { DataInput } from '../../components/achievement-item/interfaces/data-inputs.interface';
+import { style } from '@angular/animations';
 
 @Component({
     selector: 'app-archeivement',
     standalone: true,
     imports: [GeoCenterContainerComponent, AchievementItemComponent],
     templateUrl: './achievement.component.html',
+    styles: `
+        .ul-container{
+            display: grid;
+            grid-template-columns: 1fr;
+            gap: 3rem;
+            margin-top: 3rem;
+            padding:0;
+        }
+
+        @media (min-width: 640px) {
+            .ul-container {
+                grid-template-columns: repeat(2, 1fr);
+            }
+        }
+    `,
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ArcheivementComponent {
     public dataInput = signal<DataInput>({
         id: '818af8f2-80kt-484e-9fab-a3d4ad846645',
         description: '¡Eres genial! Has completado la primera categoria.',
-        name: 'Completa una categoria',
+        name: '¡Inicio!',
         image: 'assets/images/achievement/primer-logro.svg',
     });
 }
