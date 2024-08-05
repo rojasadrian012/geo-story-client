b25c20d1ef875cfa187c1e4fd568d603a666b60d
rojasadrian012 <rojasadrian012@gmail.com>
2024-08-05 19:02:44 -0400

    changes in the styles

--- src/app/demo/components/dashboard/components/achievement-item/achievement-item.component.ts
+++ src/app/demo/components/dashboard/components/achievement-item/achievement-item.component.ts
@@ -1,16 +1,42 @@
-import { CommonModule, JsonPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { DataInput } from './interfaces/data-inputs.interface';
+import { DatePipe } from '@angular/common';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    input,
+    OnInit,
+    signal,
+} from '@angular/core';
 import { AchievementCurrentUser } from '../../pages/achievement/interfaces/achievement-list-response.interface';
 
 @Component({
     selector: 'app-achievement-item',
     standalone: true,
-    imports: [JsonPipe],
+    imports: [DatePipe],
     templateUrl: './achievement-item.component.html',
     styleUrl: './achievement-item.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
+    providers: [DatePipe],
 })
-export class AchievementItemComponent {
+export class AchievementItemComponent implements OnInit {
     achievement = input.required<AchievementCurrentUser>();
+
+    dateFormated = signal<string>('');
+
+    constructor(private datePipe: DatePipe) {}
+
+    ngOnInit(): void {
+        this.formatDate();
+    }
+
+    formatDate() {
+        const achievementDate = new Date(this.achievement().date);
+        
+        const paraguayTime = new Date(
+            achievementDate.getTime() - 4 * 60 * 60 * 1000
+        );
+
+        this.dateFormated.set(
+            this.datePipe.transform(paraguayTime, 'EEEE, d \'de\' MMMM \'de\' y, H:mm:ss', undefined, 'es')
+        );
+    }
 }
