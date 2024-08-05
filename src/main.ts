708d594e48227baa9fca8e29105a756c810c3fbf
rojasadrian012 <rojasadrian012@gmail.com>
2024-08-05 19:02:14 -0400

    add locale id

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, LOCALE_ID } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+
+registerLocaleData(localeEs, 'es');
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule,{
+  providers: [{provide: LOCALE_ID, useValue: 'en-US' }]
+})
   .catch(err => console.error(err));
