5c6b880e85d2e60ade5fb080e83b859acfd84c51
rojasadrian012 <rojasadrian012@gmail.com>
2024-08-08 10:31:09 -0400

    service save achievement

--- src/app/demo/components/dashboard/pages/achievement/services/achievement-page.service.ts
+++ src/app/demo/components/dashboard/pages/achievement/services/achievement-page.service.ts
@@ -4,6 +4,20 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { AchievementListResponse } from '../interfaces/achievement-list-response.interface';
 
+enum AchievementCode {
+    CURIOUS_10 = 'CURIOUS_10',
+    PERSISTENT_15 = 'PERSISTENT_15',
+    DEDICATED_20 = 'DEDICATED_20',
+    CONSTANT_5 = 'CONSTANT_5',
+    UNSTOPPABLE_10 = 'UNSTOPPABLE_10',
+    PRECISE_5 = 'PRECISE_5',
+    EXPERT_10 = 'EXPERT_10',
+    MASTER_15 = 'MASTER_15',
+    PERFECTIONIST = 'PERFECTIONIST',
+    EARTHLY_GOD = 'EARTHLY_GOD',
+    EXPLORER = 'EXPLORER',
+    COLLECTOR = 'COLLECTOR'
+}
 @Injectable({
     providedIn: 'root'
 })
@@ -15,6 +29,10 @@ export class AchievementPageService {
 
     getAchievementsByUser(): Observable<AchievementListResponse> {
         return this.http.get<AchievementListResponse>(this.baseUrl);
+    }
+
+    saveAchievement(code: string) {
+        return this.http.post(this.baseUrl + '/save', { code });
     }
 
 }
