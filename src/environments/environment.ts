1567867cc3654d84d85cc04b10583f7e64162692
rojasadrian012 <rojasadrian012@gmail.com>
2024-08-10 12:34:07 -0400

    add path api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    baseUrl: 'http://localhost:3000/api',
+    baseUrl: 'https://geo-story-api-production.up.railway.app/api',
     number_of_questions: 5,
     timeModal: 2000
 };
