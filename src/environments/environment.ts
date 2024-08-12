177feefd80d32baf474e587693983a7112b10fce
rojasadrian012 <rojasadrian012@gmail.com>
2024-08-12 15:26:10 -0400

    spelling corrections merge

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
-    baseUrl: 'https://geo-story-api-production.up.railway.app/api',
+    baseUrl: 'http://localhost:3000/api',
+    // baseUrl: 'https://geo-story-api-production.up.railway.app/api',
     number_of_questions: 5,
     timeModal: 2000
 };
