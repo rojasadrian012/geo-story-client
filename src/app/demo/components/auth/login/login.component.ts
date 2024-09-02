ad4e159593af2e07d2033f2bc6e4761701eb17fa
rojasadrian012 <rojasadrian012@gmail.com>
2024-09-02 16:37:49 -0400

    used signals

--- src/app/demo/components/auth/login/login.component.ts
+++ src/app/demo/components/auth/login/login.component.ts
@@ -1,81 +1,76 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, model } from '@angular/core';
 import { LayoutService } from 'src/app/layout/service/app.layout.service';
 import { AuthService } from '../services/auth.service';
 
 import { MessageService } from 'primeng/api';
 import { Router } from '@angular/router';
 
-
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
     providers: [MessageService],
-    styles: [`
-        :host ::ng-deep .pi-eye,
-        :host ::ng-deep .pi-eye-slash {
-            transform:scale(1.6);
-            margin-right: 1rem;
-            color: var(--primary-color) !important;
-        }
-    `]
+    styles: [
+        `
+            :host ::ng-deep .pi-eye,
+            :host ::ng-deep .pi-eye-slash {
+                transform: scale(1.6);
+                margin-right: 1rem;
+                color: var(--primary-color) !important;
+            }
+        `,
+    ],
 })
 export class LoginComponent {
+    private messageService = inject(MessageService);
+    private authService = inject(AuthService);
+    private router = inject(Router);
+    public layoutService = inject(LayoutService);
 
     valCheck: string[] = ['remember'];
-
-    password: string = '';
-
-    nickname: string = ''
-
-    private messageService = inject(MessageService)
-
-    private authService = inject(AuthService)
-
-    private router = inject(Router)
-
-    constructor(public layoutService: LayoutService) { }
+    password = model('');
+    nickname = model('');
 
     login() {
-        this.authService.login(this.nickname, this.password)
-            .subscribe({
-                next: () => {
-                    this.router.navigateByUrl('/')
-                },
-                error: (err) => {
-                    if (Array.isArray(err) && err.length > 0) {
-
-                        if (err.length === 1) {
-                            this.messageService.add({
-                                severity: 'warn',
-                                summary: 'Error al iniciar sesión',
-                                detail: err[0] || 'Error desconocido',
-                                life: 3000
-                            });
-                        } else {
-                            err.forEach((error) => {
-                                this.messageService.add({
-                                    severity: 'error',
-                                    summary: 'Error al iniciar sesión',
-                                    detail: error || 'Error desconocido',
-                                    life: 3000
-                                });
-                            });
-                        }
-                    } else {
+        
+        this.authService.login(this.nickname(), this.password()).subscribe({
+            next: () => {
+                this.router.navigateByUrl('/');
+            },
+            error: (err) => {
+                if (Array.isArray(err) && err.length > 0) {
+                    if (err.length === 1) {
                         this.messageService.add({
-                            severity: 'error',
+                            severity: 'warn',
                             summary: 'Error al iniciar sesión',
-                            detail: err || 'Error desconocido',
-                            life: 3000
+                            detail: err[0] || 'Error desconocido',
+                            life: 3000,
+                        });
+                    } else {
+                        err.forEach((error) => {
+                            this.messageService.add({
+                                severity: 'error',
+                                summary: 'Error al iniciar sesión',
+                                detail: error || 'Error desconocido',
+                                life: 3000,
+                            });
                         });
                     }
+                } else {
+                    this.messageService.add({
+                        severity: 'error',
+                        summary: 'Error al iniciar sesión',
+                        detail: err || 'Error desconocido',
+                        life: 3000,
+                    });
                 }
-            });
+            },
+        });
     }
 
     formatNickname() {
-        this.nickname = this.nickname
-            .toLowerCase()
-            .replace(/[^a-z]/g, '');
+        this.nickname.set(
+            this.nickname().toLowerCase().replace(/[^a-z0-9]/g, '').trim()
+        );
     }
+    
 }
