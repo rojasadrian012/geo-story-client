9667421ce6e2d0cf3134f8d975a1beb9e0d5aafa
rojasadrian012 <rojasadrian012@gmail.com>
2024-09-22 15:02:55 -0400

    remove unused imports

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -3,9 +3,7 @@ import { HttpClient } from '@angular/common/http';
 
 import { environment } from 'src/environments/environment';
 
-import { DashboardService } from './services/dashboard.service';
 import { LevelByUser } from './interfaces/levels-by-user.interface';
-import { QuizStatusService } from './services/quizStatus.service';
 
 @Component({
     templateUrl: './dashboard.component.html',
@@ -13,8 +11,6 @@ import { QuizStatusService } from './services/quizStatus.service';
 })
 export class DashboardComponent implements OnInit {
     private readonly http = inject(HttpClient);
-    public dashboardService = inject(DashboardService);
-    public quizStatusService = inject(QuizStatusService);
 
     ngOnInit(): void {
         this.getQuizzes();
